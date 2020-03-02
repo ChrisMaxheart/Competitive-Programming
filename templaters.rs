2bf4cd5db2d29d0d7c7904aca1d51dc793ab7546
Maxheart <samuelhenrykurniawan@yahoo.com>
2020-03-03 01:25:13 +0800

    update template rs

--- templaters.rs
+++ templaters.rs
@@ -2,22 +2,27 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
+
+struct InputReader {
+    filename: String,
+}
+
+impl InputReader {
+    fn read(&self) -> Vec<String> {
+        if let Ok(lines) = read_lines(&self.filename) {
+            return lines.map(|x| x.unwrap()).collect()
+        }
+        return Vec::new()
+    }
+}
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-fn read_from_file(filename: String) -> Vec<String> {
-    if let Ok(lines) = read_lines(filename) {
-        return lines.map(|x| x.unwrap()).collect()
-    }
-    Vec::new()
-}
-
 fn main() {
-    let inp = read_from_file(String::from("./test.txt"));
+    let inp = InputReader{ filename:String::from("./test.txt") }.read();
 }
 
