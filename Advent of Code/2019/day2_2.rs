140cc870fa4380d3b3e792b7f01ef73a7c3a87f9
Samuel Henry Kurniawan <kurniawans@seagroup.com>
2020-03-03 14:56:24 +0800

    add day 2 part 2

---
+++ Advent of Code/2019/day2_2.rs
@@ -0,0 +1,74 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+struct InputReader {
+    filename: String,
+}
+
+impl InputReader {
+    fn read(&self) -> Vec<String> {
+        if let Ok(lines) = read_lines(&self.filename) {
+            return lines.map(|x| x.unwrap()).collect()
+        }
+        return Vec::new()
+    }
+}
+
+struct Solver {
+    data: Vec<i32>,
+    noun: i32,
+    verb: i32,
+}
+
+impl Solver {
+    fn solve(&mut self) -> i32 {
+        self.data[1] = self.noun;
+        self.data[2] = self.verb;
+        let mut pointer: usize = 0;
+        loop {
+            let command = self.data[pointer];
+            if command == 99 {
+                break;
+            }
+
+            let first_elem = self.data[self.data[pointer + 1] as usize];
+            let second_elem = self.data[self.data[pointer + 2] as usize];
+            let op = self.get_op(command);
+
+            let result_location: usize = self.data[pointer + 3] as usize;
+            self.data[result_location] = op(first_elem, second_elem);
+            pointer += 4;
+        }
+        self.data[0]
+    }
+
+    fn get_op(&self, code: i32) -> fn(i32, i32) -> i32 {
+        match code {
+            1 => |x, y| x + y,
+            _ => |x, y| x * y
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let inp = &InputReader{ filename:String::from("./test.txt") }.read()[0];
+    for verb in 0..100 {
+        for noun in 0..100 {
+            let mut solver = Solver{ data:inp.split(",").map(|x| x.parse::<i32>().unwrap()).collect(), verb, noun };
+            let ans = solver.solve();
+            if ans == 19690720 {
+                println!("{}", 100 * noun + verb);
+            }
+
+        }
+    }
+}
+
